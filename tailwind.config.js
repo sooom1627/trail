af6e4938c5ba1fcb36680e3a9779d2984cc223d6
soma <swfh2367@gmail.com>
2023-11-23 17:58:21 +0900

    design: draft layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '5/12': '41.666667%',
+      },
+      minWidth:{
+        '5/12': '41.666667%'
+      }
+    },
   },
   plugins: [],
 }
