749169f2ba8c830f81de5eb3e36a762df9b85d21
soma <swfh2367@gmail.com>
2023-11-25 19:06:01 +0900

    fix

--- src/features/task/feature/taskList/TaskCard.tsx
+++ src/features/task/feature/taskList/TaskCard.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRecoilState } from "recoil";
 import { selectedTasksState } from "../../srtores/atom/taskAtom";
 import { Task } from "../../interface/Task";
 import { getPriorityIcon } from "../../utils/getPriorityIcon";
 import { ClockIcon } from "../../../../components/icons/ClockIcon";
 import { EditIcon } from "../../../../components/icons/action/EditIcon";
-import { calculateExecutionTime } from "../../utils/calculateExecutionTime";
 
 interface TaskCardProps {
 	task: Task;
@@ -14,19 +13,6 @@ interface TaskCardProps {
 
 export const TaskCard: React.FC<TaskCardProps> = (task) => {
 	const [, setSelectedTask] = useRecoilState(selectedTasksState);
-	const [executionTime, setExecutionTime] = useState<
-		{ hoursStr: string; minutesStr: string; secondsStr: string } | undefined
-	>({ hoursStr: "00", minutesStr: "00", secondsStr: "00" });
-
-	useEffect(() => {
-		if (task.task.startTime && task.task.endTime) {
-			const result = calculateExecutionTime(
-				task.task.startTime,
-				task.task.endTime
-			);
-			setExecutionTime(result);
-		}
-	}, [task.task.startTime, task.task.endTime]);
 
 	return (
 		<div
@@ -41,7 +27,7 @@ export const TaskCard: React.FC<TaskCardProps> = (task) => {
 			</div>
 			<div className="flex items-center min-w-fit ml-4">
 				<ClockIcon />
-				<p className="text-sm ml-2 mr-4">{`${executionTime?.hoursStr}h ${executionTime?.minutesStr}m ${executionTime?.secondsStr}s`}</p>
+				<p className="text-sm ml-2 mr-4">00h 00m 00s</p>
 				{getPriorityIcon(task.task.priority)}
 				<div className="bg-zinc-100 p-1.5 rounded-full ml-6 cursol-pointer hover:bg-zinc-200">
 					<EditIcon />
--- src/features/task/utils/calculateExecutionTime.tsx
+++ src/features/task/utils/calculateExecutionTime.tsx
@@ -1,13 +1,26 @@
-export const calculateExecutionTime = (startTime: Date, endTime: Date) => {
-	const diff = endTime.getTime() - startTime.getTime();
+export const calculateExecutionTime = (
+	startTime: Date,
+	endTime?: Date,
+	status?: string
+) => {
+	let diff: number;
 
-	const hours = Math.floor(diff / (1000 * 60 * 60));
-	const minutes = Math.floor((diff / (1000 * 60)) % 60);
+	if (status === "doing") {
+		const now = new Date();
+		diff = now.getTime() - startTime.getTime();
+	} else if (status === "done" && endTime) {
+		diff = endTime.getTime() - startTime.getTime();
+	} else {
+		return;
+	}
+
+	const hours = Math.floor(diff / 1000 / 60 / 60);
+	const minutes = Math.floor((diff / 1000 / 60) % 60);
 	const seconds = Math.floor((diff / 1000) % 60);
 
-	const hoursStr = hours.toString().padStart(2, "0");
-	const minutesStr = minutes.toString().padStart(2, "0");
-	const secondsStr = seconds.toString().padStart(2, "0");
+	const hoursStr = hours < 10 ? `0${hours}` : `${hours}`;
+	const minutesStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
+	const secondsStr = seconds < 10 ? `0${seconds}` : `${seconds}`;
 
-	return { hoursStr: hoursStr, minutesStr: minutesStr, secondsStr: secondsStr };
+	return { hoursStr, minutesStr, secondsStr };
 };
