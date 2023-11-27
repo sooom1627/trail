a655f16e3e6035d3af16cea86237b860612e53e9
soma <swfh2367@gmail.com>
2023-11-27 18:04:15 +0900

    design: no data in Timer

--- src/features/task/feature/ui/TimerEmpty/TimerEmpty.tsx
+++ src/features/task/feature/ui/TimerEmpty/TimerEmpty.tsx
@@ -1,8 +1,8 @@
 export const TimerEmpty = () => {
 	return (
 		<div className="h-full w-full flex justify-center items-center">
-			<p className="font-bold text-xl">
-				Oops! Some erro happen. Please reload again.
+			<p className="font-bold text-xl text-center">
+				We have no selected Task. Create or Select task!!
 			</p>
 		</div>
 	);
