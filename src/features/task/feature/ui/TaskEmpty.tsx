f949414399d0da7f27eb18c21d23d77346fb7f38
soma <swfh2367@gmail.com>
2023-11-27 18:38:25 +0900

    desgin: task list empty design and fix in taskEditModal

---
+++ src/features/task/feature/ui/TaskEmpty.tsx
@@ -0,0 +1,7 @@
+export const TaskEmpty = () => {
+	return (
+		<div className="flex items-center justify-center overflow-hidden h-56">
+			<p className="text-xl font-bold">Let' dive on task!!</p>
+		</div>
+	);
+};
