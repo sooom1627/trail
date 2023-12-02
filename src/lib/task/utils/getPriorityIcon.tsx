37014c7580dd2d44dd4d55b28e0917cf2b98d4ec
soma <swfh2367@gmail.com>
2023-12-03 02:42:49 +0900

    function: refactor utils

--- src/lib/task/utils/getPriorityIcon.tsx
+++ src/lib/task/utils/getPriorityIcon.tsx
@@ -1,22 +1,20 @@
+import { ReactElement } from "react";
 import { MiddleIcon } from "@/components/icons/priority/MiddleIcon";
 import { HighestIcon } from "@/components/icons/priority/HighestIcon";
 import { LowestIcon } from "@/components/icons/priority/LowestIcon";
 import { HighIcon } from "@/components/icons/priority/HighIcon";
 import { LowIcon } from "@/components/icons/priority/LowIcon";
 
-export const getPriorityIcon = (priority: string) => {
-	switch (priority) {
-		case "Highest":
-			return <HighestIcon />;
-		case "High":
-			return <HighIcon />;
-		case "Medium":
-			return <MiddleIcon />;
-		case "Low":
-			return <LowIcon />;
-		case "Lowest":
-			return <LowestIcon />;
-		default:
-			return <MiddleIcon />;
-	}
+type Priority = "Highest" | "High" | "Medium" | "Low" | "Lowest";
+
+const priorityIcons: Record<Priority, ReactElement> = {
+	Highest: <HighestIcon />,
+	High: <HighIcon />,
+	Medium: <MiddleIcon />,
+	Low: <LowIcon />,
+	Lowest: <LowestIcon />,
+};
+
+export const getPriorityIcon = (priority: Priority) => {
+	return priorityIcons[priority] || <MiddleIcon />;
 };
