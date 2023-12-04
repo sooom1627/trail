287e71a0d27de1343bd6d6bbaeece22615180af0
soma <swfh2367@gmail.com>
2023-12-04 22:23:18 +0900

    fix

--- src/lib/quickTask/hooks/useLoadQuickTasks.tsx
+++ src/lib/quickTask/hooks/useLoadQuickTasks.tsx
@@ -2,14 +2,12 @@ import { QuickTask } from "../interface/QuickTask";
 import { useRecoilState } from "recoil";
 import { quickTasksState } from "../stores/quickTaskAtom";
 import { sortAndSetQuickTasksToGlobalState } from "../util/sortAndSetQuickTasksToGlobalState";
+import { getQuickTasksFromLocalStorage } from "../dataAccess/getQuickTasksFromLocalStorage";
 
 export const useLoadQuickTasks = (): [QuickTask[], () => void] => {
 	const [quickTask, setQuickTasks] = useRecoilState(quickTasksState);
 	const loadQuickTasks = () => {
-		const quickTaskString = localStorage.getItem("quickTasks");
-		let loadedQuickTasks = quickTaskString
-			? (JSON.parse(quickTaskString) as QuickTask[])
-			: [];
+		const loadedQuickTasks = getQuickTasksFromLocalStorage();
 
 		loadedQuickTasks.forEach((task) => {
 			task.created = new Date(task.created);
