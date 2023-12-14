8faf10859df2fd1a0af2dcee20d33f9ed1f0e1b0
soma <swfh2367@gmail.com>
2023-12-14 23:25:06 +0900

    fix

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -12,7 +12,7 @@ export const IndexPage: React.FC<IndexPageProps> = ({ leftChildren }) => {
 				<div className="max-w-2/3 min-w-2/3 flex flex-col">{leftChildren}</div>
 				<div className="flex flex-col gap-4 px-2 divide-y divide-zinc-400 max-w-1/3 min-w-1/3 overflow-hidden">
 					<div className="h-1/3 w-full font-bold pb-2 flex items-center justify-center">
-						何入れよう...
+						何入れよう...🧐
 					</div>
 					<div className="h-2/3 py-2 font-bold">
 						<QuickTaskPanel />
