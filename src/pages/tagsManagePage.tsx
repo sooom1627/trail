6affd994115c9a65ccf8da5cc9c29f173b34c215
soma <swfh2367@gmail.com>
2023-12-16 21:12:39 +0900

    Library: add react router dom & add task manage page

---
+++ src/pages/tagsManagePage.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/templates/Layout";
+
+export const TagsManagePage = () => {
+	return (
+		<Layout>
+			<div className="flex py-10 h-screen gap-6 w-full">
+				<p>hello</p>
+			</div>
+		</Layout>
+	);
+};
