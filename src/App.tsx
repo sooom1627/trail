c930007abc160ebd9f4d4bc993dd13be0bcb7489
soma <swfh2367@gmail.com>
2023-12-16 21:18:42 +0900

    routes: add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 import { IndexPage } from "./pages/IndexPage";
 import { TaskPage } from "./lib/task";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TagsManagePage } from "./pages/tagsManagePage";
 
 function App() {
-	return <IndexPage leftChildren={<TaskPage />} />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<IndexPage leftChildren={<TaskPage />} />} />
+				<Route path="tags" element={<TagsManagePage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
