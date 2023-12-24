9eac7ef667f7a272d293e8c75b5dcfc3957990c3
soma <swfh2367@gmail.com>
2023-12-25 00:30:39 +0900

    fix

--- src/lib/dashboard/utils/transformTasksToChartData.ts
+++ src/lib/dashboard/utils/transformTasksToChartData.ts
@@ -28,18 +28,24 @@ export const transformTasksToChartData = (tasks: Task[], tags: Tag[]): [
 
   for (const tag in groupedTasks) {
     const data: { x: string, y: number }[] = [];
+    const today = new Date()
     for (let i = SHOWDAYS; i >= 0; i--) {
       const tasksOnDate = groupedTasks[tag].filter(task => {
-        const taskDateKey = task.endTime?.toISOString().split('T')[0];
-        const currentDateKey = new Date(Date.now() - i * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
-        return taskDateKey === currentDateKey;
+        if (task.endTime) {
+          const taskDateKey = new Date(task.endTime.getTime() + 9 * 60 * 60 * 1000).toISOString().split('T')[0];
+          const currentDateKey = new Date(today.getTime() - i * 24 * 60 * 60 * 1000 + 9 * 60 * 60 * 1000).toISOString().split('T')[0];
+          return taskDateKey === currentDateKey;
+        }
+        return false;
       });
+      console.log(tasksOnDate)
       const totalDurationOnDate = calculateTotalElapsedTime(tasksOnDate);
-      const date = new Date(Date.now() - i * 24 * 60 * 60 * 1000);
+      const date = new Date(today.getTime() - i * 24 * 60 * 60 * 1000);
       const formattedDate = `${date.getMonth() + 1}/${date.getDate()}`;
       data.push({ x: formattedDate, y: totalDurationOnDate });
     }
     chartData.push({ name: tag, data });
   }
+  
   return [chartData, Array.from(colorData)];
 }
